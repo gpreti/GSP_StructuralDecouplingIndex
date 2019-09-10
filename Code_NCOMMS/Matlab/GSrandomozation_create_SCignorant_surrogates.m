52464e7fc4e4b3f426ea01d3bfe89d00168aeedd
gpreti <50864430+gpreti@users.noreply.github.com>
2019-09-10 15:52:32 +0200

    Add files via upload

--- Code_NCOMMS/Matlab/GSrandomozation_create_SCignorant_surrogates.m
+++ Code_NCOMMS/Matlab/GSrandomozation_create_SCignorant_surrogates.m
@@ -1,37 +1,28 @@
 %% Create SC-ignorant graph signal surrogates by randomization of configuration model derived-SC harmonics Fourier coefficients
 
 %% 1) Graph structure "randomization": configuration model used to generate degree-preserving graph 
-for n=1:1 %one can also repeat some times, then take the mean
-    Wran1{n}=rand_graph(Wnew);
-    Wran=Wran1;
-end
+
+Wcm=sum(Wnew,2)*sum(Wnew,2)'./sum(sum(Wnew,2));
 
 %% 2) Create surrogate signals based on configuration model graph : XrandSran
-%create average config.model SC and decompose it
-mean_Wran=zeros(360,360);
-for i=1:n
-    mean_Wran=mean_Wran+Wran{i};
-end
-mean_Wran=mean_Wran./n;
 
-%%Laplacian
-meanLran=eye(n_ROI)-mean_Wran;
+Lcm=diag(sum(Wnew,2))-Wcm;
 %%Laplacian Decomposition
-[Uran,LambdaLran] = eig(meanLran);
-[LambdaLran, IndLran]=sort(diag(LambdaLran));
-Uran=Uran(:,IndLran);
-meanMran=fliplr(Uran);
+[Ucm,LambdaLcm] = eig(Lcm);
+[LambdaLcm, IndLcm]=sort(diag(LambdaLcm));
+Ucm=Ucm(:,IndLcm);
+Mcm=fliplr(Ucm);
 
-%%Create Surrogates
+%% Create Surrogates
 nSurr=19;
-for s=1:size(data,3)
+for s=1:nsubjs_RS
     X=zX_RS(:,:,s);
     for n=1:nSurr
         %randomize sign of Fourier coefficients
-        PHIdiag=round(rand(size(meanMran,1),1));
+        PHIdiag=round(rand(size(Mcm,1),1));
         PHIdiag(PHIdiag==0)=-1;
         PHI=diag(PHIdiag);
-        XrandSran{s}(:,:,n)=meanMran*PHI*meanMran'*X; 
+        XrandSran{s}(:,:,n)=Mcm*PHI*Mcm'*X; 
         
     end
 end
